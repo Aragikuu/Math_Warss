b1bb9fc68c6adf727ad2c6bd6e76bf3feb1d11dd
Aragi <119747217+Aragikuu@users.noreply.github.com>
2023-02-17 09:54:20 -0600

    correcction

--- resources/views/inex2.blade.php
+++ resources/views/inex2.blade.php
@@ -1,91 +1,94 @@
 @extends('layaout.master')
 @section('contenido')
-    <section class="roulette-container">
+<section class="roulette-container">
 
-        <div id="selector"></div>
+    <div id="selector"></div>
 
-        <div id="roulette">
+    <div id="roulette">
 
-            <div class="roulette-section">
-                <div class="roulette-section-container">
-                    <p>Borra tu código</p>
-                </div>
+        <div class="roulette-section">
+            <div class="roulette-section-container">
+                <p>Borra tu código</p>
             </div>
-
-            <div class="roulette-section">
-                <div class="roulette-section-container">
-                    <p>Apaga tu compu</p>
-                </div>
-            </div>
-
-            <div class="roulette-section">
-                <div class="roulette-section-container">
-                    <p>Formatea tu compu</p>
-                </div>
-            </div>
-
-            <div class="roulette-section">
-                <div class="roulette-section-container">
-                    <p>Compra helado</p>
-                </div>
-            </div>
-
-            <div class="roulette-section">
-                <div class="roulette-section-container">
-                    <p>Toma un descanso</p>
-                </div>
-            </div>
-
-            <div class="roulette-section">
-                <div class="roulette-section-container">
-                    <p>¡Huevo en tu cabeza!</p>
-                </div>
-            </div>
-
-            <div class="roulette-section">
-                <div class="roulette-section-container">
-                    <p>Tira la laptop</p>
-                </div>
-            </div>
-
-            <div class="roulette-section">
-                <div class="roulette-section-container">
-                    <p>$1,00USD a tus suscriptores</p>
-                </div>
-            </div>
-
-            <div class="roulette-section">
-                <div class="roulette-section-container">
-                    <p>Cómete un snack</p>
-                </div>
-            </div>
-
-            <div class="roulette-section">
-                <div class="roulette-section-container">
-                    <p>Juega un rato</p>
-                </div>
-            </div>
-
-            <div class="roulette-section">
-                <div class="roulette-section-container">
-                    <p>¡Termina tu tarea!</p>
-                </div>
-            </div>
-
-            <div class="roulette-section">
-                <div class="roulette-section-container">
-                    <p>Vuelve a grabar</p>
-                </div>
-            </div>
-
         </div>
 
-        <div class="button-container">
-            <button type="button" id="spin">¡Girar!</button>
-            <button type="button" id="reset">Resetear</button>
+        <div class="roulette-section">
+            <div class="roulette-section-container">
+                <p>Apaga tu compu</p>
+            </div>
         </div>
 
-    </section>
+        <div class="roulette-section">
+            <div class="roulette-section-container">
+                <p>Formatea tu compu</p>
+            </div>
+        </div>
+
+        <div class="roulette-section">
+            <div class="roulette-section-container">
+                <p>Compra helado</p>
+            </div>
+        </div>
+
+        <div class="roulette-section">
+            <div class="roulette-section-container">
+                <p>Toma un descanso</p>
+            </div>
+        </div>
+
+        <div class="roulette-section">
+            <div class="roulette-section-container">
+                <p>¡Huevo en tu cabeza!</p>
+            </div>
+        </div>
+
+        <div class="roulette-section">
+            <div class="roulette-section-container">
+                <p>Tira la laptop</p>
+            </div>
+        </div>
+
+        <div class="roulette-section">
+            <div class="roulette-section-container">
+                <p>$1,00USD a tus suscriptores</p>
+            </div>
+        </div>
+
+        <div class="roulette-section">
+            <div class="roulette-section-container">
+                <p>Cómete un snack</p>
+            </div>
+        </div>
+
+        <div class="roulette-section">
+            <div class="roulette-section-container">
+                <p>Juega un rato</p>
+            </div>
+        </div>
+
+        <div class="roulette-section">
+            <div class="roulette-section-container">
+                <p>¡Termina tu tarea!</p>
+            </div>
+        </div>
+
+        <div class="roulette-section">
+            <div class="roulette-section-container">
+                <p>Vuelve a grabar</p>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="button-container">
+        <button type="button" id="spin">¡Girar!</button>
+        <button type="button" id="reset">Resetear</button>
+    </div>
+
+</section>
+<div class="cuadro">
+    Algebra
+</div>
 @endsection
 @push('scripts')
 @endpush

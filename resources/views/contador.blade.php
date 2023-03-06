810666a98f19d1e2bfb186be8f93813ffe8821dc
Aragi <119747217+Aragikuu@users.noreply.github.com>
2023-03-06 10:21:24 -0600

    equipos

--- resources/views/contador.blade.php
+++ resources/views/contador.blade.php
@@ -23,16 +23,16 @@
 
     <div class="contenedor">
         <p class="titulo">Equipo #2</p>
-        <p class="contar" id="contar">0</p>
+        <p class="contar" id="contar2">0</p>
 
         <div class="botones">
-            <btn class="incr" id="incr">
+            <btn class="incr" id="incr2">
                 <spa class="material-icons-round">add</spa>
             </btn>
-            <btn class="decr" id="decr">
+            <btn class="decr" id="decr2">
                 <spa class="material-icons-round">remove</spa>
             </btn>
-            <btn class="reset" id="reset">
+            <btn class="reset" id="reset2">
                 <spa class="material-icons-round">cached</spa>
             </btn>
         </div>
@@ -41,16 +41,16 @@
 
     <div class="contenedor">
         <p class="titulo">Equipo #3</p>
-        <p class="contar" id="contar">0</p>
+        <p class="contar" id="contar3">0</p>
 
         <div class="botones">
-            <btn class="incr" id="incr">
+            <btn class="incr" id="incr3">
                 <spa class="material-icons-round">add</spa>
             </btn>
-            <btn class="decr" id="decr">
+            <btn class="decr" id="decr3">
                 <spa class="material-icons-round">remove</spa>
             </btn>
-            <btn class="reset" id="reset">
+            <btn class="reset" id="reset3">
                 <spa class="material-icons-round">cached</spa>
             </btn>
         </div>
@@ -59,16 +59,16 @@
 
     <div class="contenedor">
         <p class="titulo">Equipo #4</p>
-        <p class="contar" id="contar">0</p>
+        <p class="contar" id="contar4">0</p>
 
         <div class="botones">
-            <btn class="incr" id="incr">
+            <btn class="incr" id="incr4">
                 <spa class="material-icons-round">add</spa>
             </btn>
-            <btn class="decr" id="decr">
+            <btn class="decr" id="decr4">
                 <spa class="material-icons-round">remove</spa>
             </btn>
-            <btn class="reset" id="reset">
+            <btn class="reset" id="reset4">
                 <spa class="material-icons-round">cached</spa>
             </btn>
         </div>

76e2f17f3d25fe8488c9f42065ef4c2f4b356a62
DavidCutz <davidcutz25cona@gmail.com>
2023-03-06 11:45:23 -0600

    cardo

--- resources/views/Cards/carta_g.blade.php
+++ resources/views/Cards/carta_g.blade.php
@@ -2,15 +2,16 @@
 <link rel="stylesheet" href="css/cartas/cartas.css">
 @section('titulo','Categoria 1')
 @section('contenido')
+
 <div class="scene scene--card">
   <div class="card">
     <div class="card__face card__face--front">
-      <img src="img/card.png" alt="">
+      <img src="img/alg.png" alt="">
       <h3>+10</h3>
     </div>
     <div class="card__face card__face--back">
       <h3>Pregunta</h3>
-      <p>¿que es algebra?</p>
+      <p>Un terreno de forma cuadrada tiene un área de 196 m2. ¿Cuántos metros de tela de alambre tiene que comprar para poder cercar los cuatro lados?</p>
       <div class="boton-modal">
         <label for="btn-modal"> Ver </label>
       </div>
@@ -21,12 +22,12 @@
 
   <div class="card">
     <div class="card__face card__face--front">
-      <img src="img/card.png" alt="">
-      <h3>+10</h3>
+      <img src="img/alg.png" alt="">
+      <h3>+5</h3>
     </div>
     <div class="card__face card__face--back">
       <h3>Pregunta</h3>
-      <p>¿que es algebra?</p>
+      <p>Un maestro carpintero y su ayudante trabajaron en una obra por seis días y ganaron $192. El maestro carpintero tiene un salario de $8 más por día que el de su ayudante. ¿Cuánto gana cada uno por día?</p>
     </div>
   </div>
 
@@ -34,12 +35,12 @@
 
   <div class="card">
     <div class="card__face card__face--front">
-      <img src="img/card.png" alt="">
-      <h3>+10</h3>
+      <img src="img/alg.png" alt="">
+      <h3>+50</h3>
     </div>
     <div class="card__face card__face--back">
       <h3>Pregunta</h3>
-      <p>¿que es algebra?</p>
+      <p>El perímetro de un rectángulo es 34 metros y su área es de 60 m2. ¿Cuánto mide el lado más grande de este rectángulo?</p>
     </div>
   </div>
 
@@ -47,24 +48,25 @@
 
   <div class="card">
     <div class="card__face card__face--front">
-      <img src="img/card.png" alt="">
-      <h3>+10</h3>
+      <img src="img/alg.png" alt="">
+      <h3>+30</h3>
     </div>
     <div class="card__face card__face--back">
       <h3>Pregunta</h3>
-      <p>¿que es algebra?</p>
+      <p>Si el cuadrado de un número positivo se disminuye en seis veces el número, el resultado es 27. Encontrar el número</p>
     </div>
   </div>
 </div>
+
 <div class="scene2 scene--card">
   <div class="card">
     <div class="card__face card__face--front">
-      <img src="img/card.png" alt="">
+      <img src="img/alg.png" alt="">
       <h3>+10</h3>
     </div>
     <div class="card__face card__face--back">
       <h3>Pregunta</h3>
-      <p>¿que es algebra?</p>
+      <p>Paty tiene 49 años y su hija 7, ¿dentro de cuantos años la edad de Paty será tres veces mayor que la de su hija?</p>
       <div class="boton-modal">
         <label for="btn-modal"> Ver </label>
       </div>
@@ -75,12 +77,12 @@
 
   <div class="card">
     <div class="card__face card__face--front">
-      <img src="img/card.png" alt="">
-      <h3>+10</h3>
+      <img src="img/alg.png" alt="">
+      <h3>+25</h3>
     </div>
     <div class="card__face card__face--back">
       <h3>Pregunta</h3>
-      <p>¿que es algebra?</p>
+      <p>Una tubería atraviesa diagonalmente un terreno de forma cuadrada. La tubería mide 30 m. ¿Cuál es la longitud, en metros, del lado del cuadrado? </p>
     </div>
   </div>
 
@@ -88,12 +90,12 @@
 
   <div class="card">
     <div class="card__face card__face--front">
-      <img src="img/card.png" alt="">
-      <h3>+10</h3>
+      <img src="img/alg.png" alt="">
+      <h3>+20</h3>
     </div>
     <div class="card__face card__face--back">
       <h3>Pregunta</h3>
-      <p>¿que es algebra?</p>
+      <p>Una familia salió a las 9:30 am con rumbo al destino donde pasarían el fin de semana y llegaron a las 17:00 hrs del mismo día. Si el auto donde viajaban se desplazó a una velocidad constante de 95 km/h, ¿Cuántos kilómetros recorrió?</p>
     </div>
   </div>
 
@@ -101,32 +103,32 @@
 
   <div class="card">
     <div class="card__face card__face--front">
-      <img src="img/card.png" alt="">
-      <h3>+10</h3>
+      <img src="img/alg.png" alt="">
+      <h3>+35</h3>
     </div>
     <div class="card__face card__face--back">
       <h3>Pregunta</h3>
-      <p>¿que es algebra?</p>
+      <p>Para encontrar el valor de un artículo deportivo se debe multiplicar el valor del artículo por su mismo valor disminuido en ocho, y esto dará como resultado 48. Encuentre el valor del artículo.</p>
     </div>
   </div>
 </div>
-<!--Ventanas Modal-->
+
+<!--Ventanas Modal1-->
 <input type="checkbox" id="btn-modal" />
 <div class="container-modal">
   <div class="content-modal">
-    <h2>¿que es algebra?</h2>
+    <h2>Respuesta</h2>
     <p class="tamaño">
-      2+2=?
+    56 m
     </p>
     <div class="btn-cerrar">
       <label for="btn-modal">Cerrar</label>
     </div>
-    <div class="btn-respuesta">
-      <label for="btn-modal">respuesta</label>
-    </div>
   </div>
   <label for="btn-modal" class="cerrar-modal"></label>
 </div>
+
+
 @endsection
 @push('scripts')
 <script src="js/categoria2/carta_g.js"></script>

76c8484bab718d9da315b1e79c90293e74a2a526
Aragi <119747217+Aragikuu@users.noreply.github.com>
2023-03-06 01:33:19 -0600

    intenti de grid

--- resources/views/Cards/carta_g.blade.php
+++ resources/views/Cards/carta_g.blade.php
@@ -1,4 +1,5 @@
 @extends('layaout.plantilla')
+<link rel="stylesheet" href="css/cartas/cartas.css">
 @section('titulo','Categoria 1')
 @section('contenido')
 <div class="scene scene--card">
@@ -10,6 +11,9 @@
     <div class="card__face card__face--back">
       <h3>Pregunta</h3>
       <p>¿que es algebra?</p>
+      <div class="boton-modal">
+        <label for="btn-modal"> Ver </label>
+      </div>
     </div>
   </div>
 </div>
@@ -52,11 +56,23 @@
     </div>
   </div>
 </div>
-
-
-
-
-
+<!--Ventanas Modal-->
+<input type="checkbox" id="btn-modal" />
+<div class="container-modal">
+  <div class="content-modal">
+    <h2>¿que es algebra?</h2>
+    <p class="tamaño">
+      2+2=?
+    </p>
+    <div class="btn-cerrar">
+      <label for="btn-modal">Cerrar</label>
+    </div>
+    <div class="btn-respuesta">
+      <label for="btn-modal">respuesta</label>
+    </div>
+  </div>
+  <label for="btn-modal" class="cerrar-modal"></label>
+</div>
 @endsection
 @push('scripts')
 <script src="js/categoria2/carta_g.js"></script>

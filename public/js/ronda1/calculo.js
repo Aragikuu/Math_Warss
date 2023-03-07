c563914dec182c16dba0315cf9f957ae1e799354
DavidCutz <davidcutz25cona@gmail.com>
2023-03-07 17:13:12 -0600

    ronda1

---
+++ public/js/ronda1/calculo.js
@@ -0,0 +1,15 @@
+function modal1(){
+    swal("Respuesta: ", "d(h)=√(h^2+100 )", "success");
+}
+
+function modal2(){
+    swal("Respuesta: ", "R_f:[0,∞]", "success");
+}
+
+function modal3(){
+    swal("Respuesta: ", "14", "success");
+}
+
+function modal4(){
+    swal("Respuesta: ", "3/√x-1/(2x^(3/2) )", "success");
+}
--- resources/views/Ronda1/Cards/carta_g.blade.php
+++ resources/views/Ronda1/Cards/carta_g.blade.php
@@ -1,12 +1,11 @@
 @extends('layaout.plantilla')
 <link rel="stylesheet" href="css/cartas/cartas.css">
-@section('titulo','Categoria 1')
+@section('titulo','Algebra')
 @section('contenido')
 
 <div class="scene scene--card">
   <div class="card">
     <div class="card__face card__face--front">
-      <img src="img/alg.png" alt="">
       <h3>+10</h3>
     </div>
     <div class="card__face card__face--back">
@@ -20,7 +19,6 @@
 
   <div class="card">
     <div class="card__face card__face--front">
-      <img src="img/alg.png" alt="">
       <h3>+5</h3>
     </div>
     <div class="card__face card__face--back">
@@ -29,43 +27,16 @@
       <button onclick="modal2()">Abrir</button>
     </div>
   </div>
-
-
-
-  <div class="card">
-    <div class="card__face card__face--front">
-      <img src="img/alg.png" alt="">
-      <h3>+50</h3>
-    </div>
-    <div class="card__face card__face--back">
-      <h3>Pregunta</h3>
-      <p>El perímetro de un rectángulo es 34 metros y su área es de 60 m2. ¿Cuánto mide el lado más grande de este rectángulo?</p>
-    </div>
-  </div>
-
-
-
-  <div class="card">
-    <div class="card__face card__face--front">
-      <img src="img/alg.png" alt="">
-      <h3>+30</h3>
-    </div>
-    <div class="card__face card__face--back">
-      <h3>Pregunta</h3>
-      <p>Si el cuadrado de un número positivo se disminuye en seis veces el número, el resultado es 27. Encontrar el número</p>
-    </div>
-  </div>
 </div>
 
 <div class="scene2 scene--card">
   <div class="card">
     <div class="card__face card__face--front">
-      <img src="img/alg.png" alt="">
       <h3>+10</h3>
     </div>
     <div class="card__face card__face--back">
       <h3>Pregunta</h3>
-      <p>Paty tiene 49 años y su hija 7, ¿dentro de cuantos años la edad de Paty será tres veces mayor que la de su hija?</p>
+      <p>El perímetro de un rectángulo es 34 metros y su área es de 60 m2. ¿Cuánto mide el lado más grande de este rectángulo?</p>
       <button onclick="modal3()">Abrir</button>
     </div>
   </div>
@@ -74,64 +45,18 @@
 
   <div class="card">
     <div class="card__face card__face--front">
-      <img src="img/alg.png" alt="">
       <h3>+25</h3>
     </div>
     <div class="card__face card__face--back">
       <h3>Pregunta</h3>
-      <p>Una tubería atraviesa diagonalmente un terreno de forma cuadrada. La tubería mide 30 m. ¿Cuál es la longitud, en metros, del lado del cuadrado? </p>
+      <p>Si el cuadrado de un número positivo se disminuye en seis veces el número, el resultado es 27. Encontrar el número.</p>
       <button onclick="modal4()">Abrir</button>
     </div>
   </div>
-
-
-
-  <div class="card">
-    <div class="card__face card__face--front">
-      <img src="img/alg.png" alt="">
-      <h3>+20</h3>
-    </div>
-    <div class="card__face card__face--back">
-      <h3>Pregunta</h3>
-      <p>Una familia salió a las 9:30 am con rumbo al destino donde pasarían el fin de semana y llegaron a las 17:00 hrs del mismo día. Si el auto donde viajaban se desplazó a una velocidad constante de 95 km/h, ¿Cuántos kilómetros recorrió?</p>
-    </div>
-  </div>
-
-
-
-  <div class="card">
-    <div class="card__face card__face--front">
-      <img src="img/alg.png" alt="">
-      <h3>+15</h3>
-    </div>
-    <div class="card__face card__face--back">
-      <h3>Pregunta</h3>
-      <p>Para encontrar el valor de un artículo deportivo se debe multiplicar el valor del artículo por su mismo valor disminuido en ocho, y esto dará como resultado 48. Encuentre el valor del artículo.</p>
-    </div>
-  </div>
-</div>
-
-<!--Ventanas Modal1-->
-<input type="checkbox" id="btn-modal" />
-<div class="container-modal">
-  <div class="content-modal">
-    <h2>Respuesta</h2>
-    <p class="tamaño">
-    56 m
-    </p>
-    <div class="btn-cerrar">
-      <label for="btn-modal">Cerrar</label>
-    </div>
-  </div>
-  <label for="btn-modal" class="cerrar-modal"></label>
-</div>
-
 
 
 @endsection
 @push('scripts')
 <script src="js/categoria2/carta_g.js"></script>
 <script src="js/ronda1/algebra.js"></script>
-<script src="js/modal.js"></script>
-<script src="js/modal2.js"></script>
 @endpush

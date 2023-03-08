2c0ef060a6802697d31ee6e193d39d2383a86e5b
Aragi <119747217+Aragikuu@users.noreply.github.com>
2023-03-08 17:16:41 -0600

    hola

--- resources/views/Ronda1/Cards/carta_g.blade.php
+++ resources/views/Ronda1/Cards/carta_g.blade.php
@@ -11,7 +11,7 @@
     <div class="card__face card__face--back">
       <h3>Pregunta</h3>
       <p>Un terreno de forma cuadrada tiene un área de 196 m2. ¿Cuántos metros de tela de alambre tiene que comprar para poder cercar los cuatro lados?</p>
-      <button onclick="modal1()">Abrir</button>
+      <button class="btn-blanco" onclick="modal1()">Abrir</button>
     </div>
   </div>
 
@@ -24,7 +24,7 @@
     <div class="card__face card__face--back">
       <h3>Pregunta</h3>
       <p>Un maestro carpintero y su ayudante trabajaron en una obra por seis días y ganaron $192. El maestro carpintero tiene un salario de $8 más por día que el de su ayudante. ¿Cuánto gana cada uno por día?</p>
-      <button onclick="modal2()">Abrir</button>
+      <button class="btn-blanco" onclick="modal2()">Abrir</button>
     </div>
   </div>
 </div>
@@ -37,7 +37,7 @@
     <div class="card__face card__face--back">
       <h3>Pregunta</h3>
       <p>El perímetro de un rectángulo es 34 metros y su área es de 60 m2. ¿Cuánto mide el lado más grande de este rectángulo?</p>
-      <button onclick="modal3()">Abrir</button>
+      <button class="btn-blanco" onclick="modal3()">Abrir</button>
     </div>
   </div>
 
@@ -50,11 +50,11 @@
     <div class="card__face card__face--back">
       <h3>Pregunta</h3>
       <p>Si el cuadrado de un número positivo se disminuye en seis veces el número, el resultado es 27. Encontrar el número.</p>
-      <button onclick="modal4()">Abrir</button>
+      <button class="btn-blanco" onclick="modal4()">Abrir</button>
     </div>
   </div>
 
-
+</div>
 @endsection
 @push('scripts')
 <script src="js/categoria2/carta_g.js"></script>

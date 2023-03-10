cf36d7471ddb9de13863a5cdd0c4a1291a88c28b
FranciscoTamay <91625507+FranciscoTamay@users.noreply.github.com>
2023-03-10 09:33:45 -0600

    cambios

--- resources/views/algebra/algebra.blade.php
+++ resources/views/algebra/algebra.blade.php
@@ -8,7 +8,7 @@
 <div class="scene scene--card">
     
 @foreach($algebra as $row)
-@php $repuesta = $row->respuesta @endphp
+
   <div class="card">
     <div class="card__face card__face--front">
       <h3 class="puntos">{{$row->puntos}}</h3>
@@ -16,9 +16,9 @@
     <div class="card__face card__face--back">
       <h3 class="preguntas">Pregunta</h3>
       <p class="texto_pregunta">{{$row->pregunta}}</p>
-      <p class="texto_pregunta" hidden>{{$row->respuesta}}</p>
+      <p class="texto_pregunta" >{{$row->respuesta}}</p>
 
-      <button type="submit" class="btn-blanco" onclick="respuesta()">Abrir</button>
+      <button type="submit" class="btn-blanco" onclick="respuesta('<?php echo $row['id'] ?>', '<?php echo $row['respuesta'] ?>')">Abrir</button>
     </div>
   </div>
 
@@ -34,9 +34,9 @@
 <script src="js/ronda1/calculo.js"></script>
 <script src="js/mensajes/mensajes.js"></script>
 <script>
-    function respuesta(){
+    function respuesta(id,respusta){
     
-    swal("Respuesta: ", "{{$row->respuesta}}", "success");
+    swal("Respuesta: ", "<?php echo $row['respuesta'] ?>", "success");
 }
 </script>
 @endpush

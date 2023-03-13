be9d25e6b92bb8d592ac38130d1610f64cfc2d7e
DavidCutz <davidcutz25cona@gmail.com>
2023-03-13 08:33:29 -0600

    estadistica

--- public/js/ronda1/algebra.js
+++ public/js/ronda1/algebra.js
@@ -1,15 +1,15 @@
 function modal1(){
-    swal("Respuesta: ", "56 m", "success");
+    swal("56 m", "", "success");
 }
 
 function modal2(){
-    swal("Respuesta: ", "Ayudante 12, carpintero 20", "success");
+    swal("Ayudante 12, carpintero 20", "", "success");
 }
 
 function modal3(){
-    swal("Respuesta: ", "12 m", "success");
+    swal("12 m", "", "success");
 }
 
 function modal4(){
-    swal("Respuesta: ", "9", "success");
+    swal("9", "", "success");
 }
--- resources/views/RondaF/Cards/card5_g3.blade.php
+++ resources/views/RondaF/Cards/card5_g3.blade.php
@@ -11,7 +11,7 @@
     </div>
     <div class="card__face card__face--back">
       <h3 class="preguntas">Pregunta</h3>
-      <p class="texto_pregunta"></p>
+      <p class="texto_pregunta">En una clínica infantil se ha ido anotando, durante un mes, el número de metros que cada niño anda, seguido y sin caerse, el primer día que comienza a caminar, obteniéndose la tabla de información adjunta. Hallar la varianza y desviación estándar:</p>
       <button class="btn-card" onclick="modal1()">Abrir</button>
     </div>
   </div>
@@ -22,7 +22,10 @@
     </div>
     <div class="card__face card__face--back">
       <h3 class="preguntas">Pregunta</h3>
-      <p class="texto_pregunta"></p>
+      <p class="texto_pregunta2">Se ha realizado una encuesta en 30 hogares en la que se les pregunta el N.º de individuos que conviven en el domicilio habitualmente. Las respuestas obtenidas han sido las siguientes:
+      4, 4, 1, 3, 5, 3, 2, 4, 1, 6, 2, 3, 4, 5, 5, 6, 2, 3, 3, 2, 2, 1, 8, 3, 5, 3, 4, 7, 2, 3.
+      ¿Qué proporción de hogares está compuesto por tres o menos personas? 
+      </p>
       <button class="btn-card" onclick="modal2()">Abrir</button>
     </div>
   </div>
@@ -35,7 +38,8 @@
     </div>
     <div class="card__face card__face--back">
       <h3 class="preguntas">Pregunta</h3>
-      <p class="texto_pregunta"></p>
+      <p class="texto_pregunta">De los siguientes datos encuentre la media, moda y mediana </p>
+      <img class="imageness" src="img/Ronda3/estadistica3.png" alt="">
       <button class="btn-card" onclick="modal3()">Abrir</button>
     </div>
   </div>
@@ -46,7 +50,8 @@
     </div>
     <div class="card__face card__face--back">
       <h3 class="preguntas">Pregunta</h3>
-      <p class="texto_pregunta"></p>
+      <p class="texto_pregunta">Encuentre la varianza y el coeficiente de variación de la siguiente tabla</p>
+      <img class="imageness" src="img/Ronda3/estadistica4.png" alt="">
       <button class="btn-card" onclick="modal4()">Abrir</button>
     </div>
   </div>
